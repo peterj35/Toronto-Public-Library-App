7377216d14867d421df31c8b87246de2bb693466
Peter <peterju35@gmail.com>
2015-11-03 12:23:26 -0500

    lots of analytics

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -5,32 +5,42 @@ $(document).ready(function() {
   var $library    =  $('.library'),
       $programs   =  $('.programs'),
       $graphtype  =  $('.graphtype'),
-      $graph      =  $('.graph');
+      $graph      =  $('.graph'),
+      $libraryinc   = $('.libraryinc'),
+      $programsinc  = $('.programsinc'),
+      $graphinc     = $('.graphinc');
+
 
 
   $('.malvernbtn').click(function(){
     $programs.hide();
-    $graphtype.hide();
     $graph.hide();
     $library.show();
   });
 
   $('.eslbtn').click(function(){
-    $graphtype.hide();
-    $graph.hide();
-    $programs.show();
-  });
-
-  $('.participantbtn').click(function(){
-    $graphtype.show();
     $graph.show();
+    $programs.show();
   });
 
   $('.resetbtn').click(function(){
     $library.hide();
     $programs.hide();
-    $graphtype.hide();
     $graph.hide();
   });
+
+  $('.agincourtbtn').click(function(){
+    $libraryinc.show();
+    $programsinc.hide();
+    $graphinc.hide();
+  });
+
+  $('.babytimebtn').click(function(){
+    $programsinc.show();
+  })
+
+  $('.incidencebtn').click(function(){
+    $graphinc.show();
+  })
 
 });
