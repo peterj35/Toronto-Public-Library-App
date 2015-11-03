dad6ae39a1425f38652e712c264089f3478897db
Brandon <abrandon@ca.ibm.com>
2015-11-02 20:39:29 -0500

    Poopulate catalog list

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Event = require("../schemas/schemas")
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-    res.render('catalog');
+    Event.find(function(err, events) {
+    	if (err) {
+    		console.log(err)
+    	}
+    	console.log(events)
+    	res.render('catalog', {
+    		programs: events
+    	})
+    })
 });
 
 module.exports = router;
