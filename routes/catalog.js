6b44d650fe54145b8bfc8933b5fc279827ef5e3f
Peter <peterju35@gmail.com>
2015-11-02 20:04:24 -0500

    dling watson shit and getting rid of config files

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,7 +1,4 @@
 var express = require('express');
-var knex = require('../config/knex.js');
-var DB = require('../config/DB.js');
-var Utils = require('../config/Utils')
 var router = express.Router();
 
 /* GET about page. */
